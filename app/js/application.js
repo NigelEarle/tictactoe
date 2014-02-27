d509f31f0281fd136e331fe4b69b2518a1a36e9b
WDI <nwre87@gmail.com>
2014-02-27 17:26:04 +0800

    Added new players.

--- app/js/application.js
+++ app/js/application.js
@@ -27,12 +27,13 @@
       this.startGame = __bind(this.startGame, this);
       this.resetBoard();
       this.$scope.mark = this.mark;
-      this.scope.startGame = this.startGame;
-      this.scope.gameOn = false;
+      this.$scope.startGame = this.startGame();
+      this.$scope.gameOn = false;
     }
 
     BoardCtrl.prototype.startGame = function() {
-      this.scope.gameOn = true;
+      this.$scope.gameOn = true;
+      this.$scope.currentPlayer = this.player();
       return this.resetBoard();
     };
 
@@ -56,7 +57,10 @@
     };
 
     BoardCtrl.prototype.resetBoard = function() {
+      this.$scope.theWinnerIs = false;
+      this.$scope.cats = false;
       this.cells = this.$scope.cells = {};
+      this.$scope.currentPlayer = this.player();
       return this.getPatterns();
     };
 
@@ -122,12 +126,12 @@
       winner = this.player({
         whoMovedLast: true
       });
-      alert("" + winner + " wins!");
+      this.$scope.theWinnerIs = winner;
       return this.$scope.gameOn = false;
     };
 
     BoardCtrl.prototype.announceTie = function() {
-      alert("It's a tie!");
+      this.$scope.cats = true;
       return this.$scope.gameOn = false;
     };
 
@@ -156,9 +160,12 @@
     BoardCtrl.prototype.mark = function($event) {
       var cell;
       this.$event = $event;
-      cell = this.$event.target.dataset.index;
-      this.cells[cell] = this.player();
-      return this.parseBoard();
+      if (this.$scope.gameOn) {
+        cell = this.$event.target.dataset.index;
+        this.cells[cell] = this.player();
+        this.parseBoard();
+        return this.$scope.currentPlayer = this.player();
+      }
     };
 
     return BoardCtrl;
